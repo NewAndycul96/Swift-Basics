90ed2d8408c94ecdfb695d571cc490e3a415fc3c
Anand Kulkarni <anandkulkarni@Anands-MacBook-Pro.local>
2018-06-15 11:09:29 -0500

    First Commit

---
+++ Swift Basics/main.swift
@@ -0,0 +1,23 @@
+//
+//  main.swift
+//  Swift Basics
+//
+//  Created by Anand Kulkarni on 6/15/18.
+//  Copyright © 2018 Anand Kulkarni. All rights reserved.
+//
+
+import Foundation
+
+var sample1: UInt8 = 0x3A
+var sample2: UInt8 = 58
+var heartRate: Int = 85
+var deposits: Double = 135002796
+let accelaration: Float = 9.800
+let mass: Float = 14.6
+var distance: Double = 129.763001
+var lost: Bool = true
+var expensive: Bool = true
+var choice: Int = 2
+let intergral: Character = "\u{222B}"
+let greeting: String = "Hello"
+var name: String = "Karen"
